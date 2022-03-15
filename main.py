0f5a82b0647ab5201fb9a2a477e0411d080ec1e1
SauleStan <vogellietuva@gmail.com>
2022-03-15 16:11:57 +0200

    Fitness score of entire generation based on non-attacking queens pairs

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 from board import Board
-from Queens import initGen
+from Queens import *
 
 if __name__ == "__main__":
     boardLength = 8
     board = Board(boardLength)
 
     generation = initGen(boardLength, 10)
+    # generation = [[3, 2, 4, 1, 2, 3, 5, 1]]
     print(generation)
     print("\n")
+    board.displayBoard(generation[0])
+
+    # Get max amount of non-attacking pairs of queens
+    max = maxFitness(boardLength)
+
+    # Provide the current generation and max possible fitness score
+    fitnessMatrix = fitness(generation, max)
+    print(fitnessMatrix)

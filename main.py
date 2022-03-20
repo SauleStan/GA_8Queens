b3bb4e7232adf7eca88ade8a71628ffd91157294
SauleStan <vogellietuva@gmail.com>
2022-03-20 15:09:47 +0200

    Runs until solution is found

--- main.py
+++ main.py
@@ -6,23 +6,28 @@ from Fitness import *
 if __name__ == "__main__":
     boardLength = 8
     gen_size = 10
+    gen_count = 1
     board = Board(boardLength)
     mutation_rate = 0.1
 
-    generation = initGen(boardLength, gen_size)
-    print(generation)
-
     # Get max amount of non-attacking pairs of queens
-    max = maxFitness(boardLength)
+    max_fitness = maxFitness(boardLength)
 
-    # Provide the current generation and max possible fitness score
-    fitnessMatrix = fitness(generation, max)
-    print(fitnessMatrix)
-    
-    parents = selection(generation, fitnessMatrix, max)
-    children = crossover(parents, mutation_rate, boardLength)
-    fitness_children = fitness(children, max)
-    generation, fitness_matrix = population_reduction(children, fitness_children, gen_size)
-    print(generation)
-    print(fitness_matrix)
+    # Initial generation
+    generation = initGen(boardLength, gen_size)
 
+    # Get fitness matrix of the initial gen
+    fitness_matrix = fitness(generation, max_fitness)
+
+    # While solution is not found, proceed to new generation    
+    while(True):
+        gen_count+=1
+        parents = selection(generation, fitness_matrix, max_fitness)
+        children = crossover(parents, mutation_rate, boardLength)
+        fitness_children = fitness(children, max_fitness)
+        generation, fitness_matrix = population_reduction(children, fitness_children, gen_size)
+        if(max_fitness in fitness_matrix):
+            solution = generation[fitness_matrix.index(max_fitness)]
+            board.displayBoard(solution)
+            print("Generation count: ", gen_count)
+            break

69e84b1ad586e6097978d4663e68c87040c016c2
SauleStan <vogellietuva@gmail.com>
2022-03-28 09:08:57 +0300

    Stats

--- main.py
+++ main.py
@@ -12,7 +12,12 @@ if __name__ == "__main__":
     gen_size = int(input("Enter generation size: "))
     mutation_rate = float(input("Enter mutation rate (from 0 to 1, use . to seperate, e.g. 0.1): "))
     run_count = int(input("How many times would you like to run the program? "))
-    
+
+    # Performance statistics
+    avg_gen_count = 0
+    best_performance = 100000
+    worst_performance = 0
+
     for _ in range(run_count):
         # Generation counter
         gen_count = 1
@@ -33,13 +38,28 @@ if __name__ == "__main__":
             children = crossover(parents, mutation_rate, board_length)
             fitness_children = fitness(children, max_fit)
             generation, fitness_matrix = population_reduction(children, fitness_children, gen_size)
+
             if(max_fit in fitness_matrix):
+                solution = generation[fitness_matrix.index(max_fit)]
+                # Set best performance
+                if (best_performance > gen_count):
+                    best_performance = gen_count
+                # Set worst performance
+                if (worst_performance < gen_count):
+                    worst_performance = gen_count
+
                 if(run_count == 1):
-                    solution = generation[fitness_matrix.index(max_fit)]
                     board.displayBoard(solution)
                     print("Generation count: ", gen_count)
                     break
                 else:
-                    solution = generation[fitness_matrix.index(max_fit)]
                     print("Generation count: ", gen_count, "Solution state: ", solution)
+                    avg_gen_count += gen_count
                     break
+    
+    # Stats
+    if(run_count != 1):
+        print("\n")
+        print("Average generation count: ", avg_gen_count/run_count)
+        print("Least generations: ", best_performance)
+        print("Most generations: ", worst_performance)
